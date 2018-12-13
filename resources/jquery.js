807e1579139b6b63816ce1723be9cbd7bd4b3b61
unknown <priesjos000@warren.k12.in.us>
2018-12-12 19:48:52 -0500

    additional paramater for animate function

--- resources/jquery.js
+++ resources/jquery.js
@@ -1,20 +1,31 @@
 $(document).ready(function(){
 
     $("#base-box-1").mouseenter(function(){
-        animateFromTop("#base-box-1");
+        animateFromTop("#base-box-1", "50");
     });
 
     $("#base-box-2").mouseenter(function(){
-        animateFromTop("#base-box-2");
+        animateFromTop("#base-box-2", "50");
     });
 
     $("#base-box-3").mouseenter(function(){
-        animateFromTop("#base-box-3");
+        animateFromTop("#base-box-3", "50");
     });
 
+    $("#base-box-1").mouseleave(function(){
+        animateFromTop("#base-box-1", "-0");
+    });
+
+    $("#base-box-2").mouseleave(function(){
+        animateFromTop("#base-box-2", "-0");
+    });
+
+    $("#base-box-3").mouseleave(function(){
+        animateFromTop("#base-box-3", "-0");
+    });
     
 })
 
-function animateFromTop(content){
-    $(content).animate({marginTop: "50"});
+function animateFromTop(content, amount){
+    $(content).animate({marginTop: amount});
 }
