a94165f82d244d1293dea9aa492ec5affa24fd2d
unknown <priesjos000@warren.k12.in.us>
2018-12-18 22:37:41 -0500

    all that's needed are fonts and jquery effects

--- resources/jquery.js
+++ resources/jquery.js
@@ -13,26 +13,32 @@ $(document).ready(function(){
 
     $("#base-box-1").mouseenter(function(){
         animateFromTop("#base-box-1", "40");
+        $("#first-notVis").fadeIn();
     });
 
     $("#base-box-2").mouseenter(function(){
         animateFromTop("#base-box-2", "40");
+        $("#second-notVis").fadeIn();
     });
 
     $("#base-box-3").mouseenter(function(){
         animateFromTop("#base-box-3", "40");
+        $("#third-notVis").fadeIn();
     });
 
     $("#base-box-1").mouseleave(function(){
         animateFromTop("#base-box-1", "-0");
+        $("#first-notVis").fadeOut();
     });
 
     $("#base-box-2").mouseleave(function(){
         animateFromTop("#base-box-2", "-0");
+        $("#second-notVis").fadeOut();
     });
 
     $("#base-box-3").mouseleave(function(){
         animateFromTop("#base-box-3", "-0");
+        $("#third-notVis").fadeOut();
     });
 });
 
